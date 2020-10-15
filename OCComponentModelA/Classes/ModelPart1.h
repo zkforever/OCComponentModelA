d21f7a65860305e9ee0c618452d3699402a2d396
zkforever <owenismylove@gmail.com>
2020-10-15 17:56:24 +0800

    添加

---
+++ OCComponentModelA/Classes/ModelPart1.h
@@ -0,0 +1,19 @@
+//
+//  ModelPart1.h
+//  OCComponentModelA
+//
+//  Created by jinxixin on 2020/10/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ModelPart1 : NSObject
+
+- (void)testModelPart1:(NSString*)str;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OCComponentModelA/Classes/ModelPart1.m
@@ -0,0 +1,16 @@
+//
+//  ModelPart1.m
+//  OCComponentModelA
+//
+//  Created by jinxixin on 2020/10/15.
+//
+
+#import "ModelPart1.h"
+
+@implementation ModelPart1
+
+- (void)testModelPart1:(NSString*)str {
+    NSLog(@"testModelPart1=%@",str);
+}
+
+@end
